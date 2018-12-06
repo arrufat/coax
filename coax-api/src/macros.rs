480827a015e7274b16783cfcf0430a38541f1d68
Oleksii Filonenko <brightone@protonmail.com>
2018-12-06 11:11:51 +0200

    Fix coax-api build, apply rustfmt
    Stable Rust (1.30.1)

--- coax-api/src/macros.rs
+++ coax-api/src/macros.rs
@@ -14,7 +14,7 @@ macro_rules! json_str_type {
             }
 
             pub fn replicate(&self) -> $name {
-                $name::new(self.0.borrow())
+                $name::new(String::from(self.0.borrow()))
             }
 
             pub fn acquire<'b>(&'a self) -> $name<'b> {
@@ -29,7 +29,7 @@ macro_rules! json_str_type {
         }
 
         impl<'a> FromJson for $name<'a> {
-            fn decode<I: Iterator<Item=char>>(d: &mut Decoder<I>) -> DecodeResult<Self> {
+            fn decode<I: Iterator<Item = char>>(d: &mut Decoder<I>) -> DecodeResult<Self> {
                 Ok($name(Cow::Owned(d.string()?)))
             }
         }
@@ -39,9 +39,8 @@ macro_rules! json_str_type {
                 self.0.fmt(f)
             }
         }
-    }
+    };
 }
-
 
 #[macro_export]
 macro_rules! json_bytes_type {
@@ -66,39 +65,38 @@ macro_rules! json_bytes_type {
         }
 
         impl<'a> FromJson for $name<'a> {
-            fn decode<I: Iterator<Item=char>>(d: &mut Decoder<I>) -> DecodeResult<Self> {
+            fn decode<I: Iterator<Item = char>>(d: &mut Decoder<I>) -> DecodeResult<Self> {
                 match ::base64::decode(d.string()?.as_str()) {
-                    Ok(x)  => Ok($name(Cow::Owned(x))),
-                    Err(e) => Err(DecodeError::Other(Box::new(e)))
+                    Ok(x) => Ok($name(Cow::Owned(x))),
+                    Err(e) => Err(DecodeError::Other(Box::new(e))),
                 }
             }
         }
-    }
+    };
 }
 
 macro_rules! from_some_ok {
     ($name:ident, $err:expr) => {
         match $name {
-            Some(Ok(x))  => x,
+            Some(Ok(x)) => x,
             Some(Err(e)) => return Err(e),
-            None         => return Err($err)
+            None => return Err($err),
         }
     };
     ($name:ident) => {
         match $name {
-            Some(Ok(x))  => Some(x),
+            Some(Ok(x)) => Some(x),
             Some(Err(e)) => return Err(e),
-            None         => None
+            None => None,
         }
-    }
+    };
 }
 
 macro_rules! from_some {
     ($name:ident, $err:expr) => {
         match $name {
             Some(x) => x,
-            None    => return Err($err)
+            None => return Err($err),
         }
-    }
+    };
 }
-
